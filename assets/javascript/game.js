be58dcc84f070c9114f85543d316908211a7e90e
Fermenting <realferalxanderyell@gmail.com>
2018-11-13 00:40:36 -0800

    created file system, transposed instructions to code and pseudo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+
+
+// da goal of the whole game need be defined. let's start it at zero.
+var targetValue = 0;
+
+// each of dem crystalz is gonna need a value too.
+
+var alphaValue = 0;
+var betaValue = 0;
+var gammaValue = 0;
+var zappaValue = 0;
+
+// When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
+
+var playerScore = 0;
+
+// on click of a crystal:
+
+// set targetValue
+// display .target { display: inline }
+// set values for alpha,beta,gamma,zappa
+//  add value to player score depending on crystal clicked.
+
+
+// logic
+
+// if (playerScore === targetValue) {WIN}
+// if (playerScore > targetValue) {GAME OVER}
+
+// game restarts
+
+// display counters for wins & losses
+// set new targetValue
+// scramble values for alpha,beta,gamma,zappa
+
+//range for targetValue
+Math.floor(Math.random()*100) + 20
+
+//range for crystals
+Math.floor(Math.random()*12) + 1
+
+
