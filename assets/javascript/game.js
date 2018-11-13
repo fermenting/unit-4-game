86e0fadd08f875469678059b54155e4af35ff49a
Fermenting <realferalxanderyell@gmail.com>
2018-11-13 02:09:39 -0800

    mathematical relationships set up and some logic drafted

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,3 @@
-
-
 // da goal of the whole game need be defined. let's start it at zero.
 var targetValue = 0;
 
@@ -10,13 +8,34 @@ var betaValue = 0;
 var gammaValue = 0;
 var zappaValue = 0;
 
+//range for crystals
+
+alphaValue = Math.floor(Math.random()*11) + 2;
+console.log(alphaValue);
+
+betaValue = Math.floor(Math.random()*11) + 2;
+console.log(betaValue);
+
+gammaValue = Math.floor(Math.random()*11) + 2;
+console.log(gammaValue);
+
+zappaValue = Math.floor(Math.random()*11) + 2;
+console.log(zappaValue);
+
 // When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
 
 var playerScore = 0;
 
 // on click of a crystal:
 
+  
 // set targetValue
+//range for targetValue
+targetValue = Math.floor(Math.random()*100) + 20;
+console.log (targetValue);
+
+
+
 // display .target { display: inline }
 // set values for alpha,beta,gamma,zappa
 //  add value to player score depending on crystal clicked.
@@ -33,10 +52,6 @@ var playerScore = 0;
 // set new targetValue
 // scramble values for alpha,beta,gamma,zappa
 
-//range for targetValue
-Math.floor(Math.random()*100) + 20
 
-//range for crystals
-Math.floor(Math.random()*12) + 1
 
 
